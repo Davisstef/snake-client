b27dd995bc7490e130f6f947a99216717c9cb4e5
vagrant <stefan.davis@mail.utoronto.ca>
2021-11-05 03:36:01 +0000

    Updates

--- snake-client/play.js
+++ snake-client/play.js
@@ -11,24 +11,6 @@ const game = new Game(new UserInterface(), new RemoteInterface() )
 game.start()
 
 const net = require("net")
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-
-  conn.setEncoding("utf8");
-  conn.on('connect', () => {
-    console.log('Success!');
-    conn.write('Name: RAS');
-  });
-
-  conn.on('data', (data) => {
-    console.log('PSA FROM MR T. FOOL', data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
